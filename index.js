78aabba85608f3a66bf92c8a4868470069a6ce33
Kristen <kgrace61@gmail.com>
2024-02-25 23:02:29 -0600

    completes lab

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
 function fetchBooks() {
-  // To pass the tests, don't forget to return your fetch!
-  
+ return fetch('https://anapioficeandfire.com/api/books')
+  .then(res => res.json())
+  .then(books => {
+    console.log(books);
+    renderBooks(books)
+  })
 }
 
+
+  
 function renderBooks(books) {
   const main = document.querySelector('main');
   books.forEach(book => {
@@ -10,8 +16,9 @@ function renderBooks(books) {
     h2.innerHTML = book.name;
     main.appendChild(h2);
   });
-}
+
 
 document.addEventListener('DOMContentLoaded', function() {
   fetchBooks();
 });
+}
